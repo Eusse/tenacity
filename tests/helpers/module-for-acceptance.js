8eb836fc7db0df9ea6971c700d9c7ffa8a1be799
Andres Eusse <carloseusse@gmail.com>
2016-07-09 10:33:32 -0500

    Changed test framework from qunit to mocha

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -1,4 +1,4 @@
-import { module } from 'qunit';
+import { module } from 'mocha';
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
